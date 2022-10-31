98fd5d006d08bdf3d7d799ffe7b8071eeeb7a7ac
Amit Kumar <72943676+oCXMo@users.noreply.github.com>
2022-10-31 22:38:47 +0530

    Update library.sql

--- library.sql
+++ library.sql
@@ -1,4 +1,4 @@
---Library Management System
+----Library Management System----
 
 --Description:
 --We need to store a lot of information in the library database: return dates, members' names, books available, and much more. For this purpose, we should create tables and match their columns with the correct data types.
